89f8efdb2daf525bbd815b6b9e87f056789f1b53
MannShah17 <mannshah2004@gmail.com>
2023-10-27 16:11:26 +0530

    done

--- models/currencyModel.js
+++ models/currencyModel.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose');
 
 const currencySchema = new mongoose.Schema({
-  code: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  name: String,
-  exchangeRate: {
-    type: Number,
-    required: true,
-  },
+  // Field 1: Define a property for 'code' with a String data type and required true
+  // Field 2: Define a property for 'name' with a String data type
+  // Field 3: Define a property for 'exchangeRate' with a Number data type and required true
 });
 
 const Currency = mongoose.model('Currency', currencySchema);
